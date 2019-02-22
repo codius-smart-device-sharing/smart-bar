4479b0ea9174bac80d8e221dc81301b0d4c70b78
Joe MacLennan <maclennan.joe@gmail.com>
2019-02-22 11:11:58 -0500

    rewrite stepper test

--- Pi_Code/stepper_5v.h
+++
@@ -1,4 +0,0 @@
-int a1 = 26;
-int a2 = 19;
-int b1 = 13;
-int b2 = 6;
--- Pi_Code/stepper_test.cpp
+++ Pi_Code/stepper_test.cpp
@@ -1,61 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <vector>
+#include <queue>
+using namespace std;
 
 int a1 = 26;
 int a2 = 19;
-int b1 = 13;
-int b2 = 6;
-int array[4][8] = {{true, false, false, false}, 
-		   {true, true, false, false},
-		   {false, true, false, false},
-		   {false, true, true, false},
-		   {false, false, true, false},
-		   {false, false, true, true},
-		   {false, false, false, true},
-		   {true, false, false, true}};
+int a3 = 13;
+int a4 = 6;
 
-void set_pins(bool array[4])
+void step(vector<bool> sequence)
 {
-	if (a)
+	if (sequence[0])
 	{
 		digitalWrite(a1, HIGH);
 	}
 	else
 	{
 		digitalWrite(a1, LOW);
-	}	
-
-	if (b)
+	}
+	
+	if (sequence[1])
 	{
 		digitalWrite(a2, HIGH);
 	}
 	else
 	{
 		digitalWrite(a2, LOW);
+	}	
+	
+	if (sequence[2])
+	{
+		digitalWrite(a3, HIGH);
 	}
+	else
+	{
+		digitalWrite(a3, LOW);
+	}	
+	
+	if (sequence[3])
+	{
+		digitalWrite(a4, HIGH);
+	}
+	else
+	{
+		digitalWrite(a4, LOW);
+	}
+}
 
-	if (c)
-	{
-		digitalWrite(b1, HIGH);
-	}
-	else
-	{
-		digitalWrite(b1, LOW);
-	}
-	
-	if (d)
-	{
-		digitalWrite(b2, HIGH);
-	}
-	else
-	{
-		digitalWrite(b2, LOW);
-	}
-	
-int main(void)
+void forward(queue<vector<bool>> sequences, int rotations)
 {
-	int step_count = 0;	
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < rotations; i++)
 	{
-		sequence(
+		for (int j = 0; j < 8; j++)
+		{
+			sequence = sequences.front();
+			sequences.pop();
+			sequences.push(sequence);
+			step(sequence);
+		}
+	}
+}
+
+int main() 
+{
+	queue<vector<bool>> sequences;
+	
+	sequences.push(vector<bool> {true, false, false, false})
+	sequences.push(vector<bool> {true, true, false, false})
+	sequences.push(vector<bool> {false, true, false, false})
+	sequences.push(vector<bool> {false, true, true, false})
+	sequences.push(vector<bool> {false, false, true, false})
+	sequences.push(vector<bool> {false, false, true, true})
+	sequences.push(vector<bool> {false, false, false, true})
+	sequences.push(vector<bool> {true, false, false, true})
+
+	pinMode(a1, OUTPUT);
+	pinMode(a2, OUTPUT);
+	pinMode(a3, OUTPUT);
+	pinMode(a4, OUTPUT);
+
+	forward(sequences, 100);
+
+	return 0;
+}
