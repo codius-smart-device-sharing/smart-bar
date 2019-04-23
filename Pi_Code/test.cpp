507eb6a98fd6b17f7ed720ed2c36333d661866e4
Joe MacLennan <maclennan.joe@gmail.com>
2019-04-23 13:14:28 -0400

    Refactored for full testing of system

--- Pi_Code/test.cpp
+++ Pi_Code/test.cpp
@@ -1,170 +1,227 @@
-#include "Pinout.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-#include "Stepper.h"
-#include "Stepper_5v.h"
-#include <wiringPi.h>
+#include "SmartBar.h"
 
-bool isOpen = true;
-/* Motor Declarations */
-Stepper_5v clamp("Clamp", CLAMP_1, CLAMP_2, CLAMP_3, CLAMP_4);
-Stepper_5v eject("Ejector", EJECT_1, EJECT_2, EJECT_3, EJECT_4);
-Stepper x("X", DIR_X, STEP_X);
-Stepper y("Y", DIR_Y, STEP_Y);
-/* Rotation Motor */
-
-void eject_cup()
-{
-	eject.move(1, 50);
-}
-
-void move_clamp(std::string action)
-{
-	if (action == "Open" && !isOpen)
-	{
-		clamp.move_5v(1, 50);
-		isOpen = true;
-	}
-	else if (action == "Close" && isOpen)
-	{
-		clamp.move_5v(0, 50);
-		isOpen = false;
-	}
-}
-
-void activate_pump(int pump_num/*, int amount*/)
-{
-	int del = 95;
-	switch (pump_num)
-	{
-		case 0: // 000 Spirit1
-			digitalWrite(SEL2, LOW);
-			digitalWrite(SEL1, LOW);
-			digitalWrite(SEL0, LOW);
-			break;
-
-		case 1: // 001 Spirit2
-			digitalWrite(SEL2, LOW);
-			digitalWrite(SEL1, LOW);
-			digitalWrite(SEL0, HIGH);
-			break;
-
-		case 2: // 010 Spirit3
-			digitalWrite(SEL2, LOW);
-			digitalWrite(SEL1, HIGH);
-			digitalWrite(SEL0, LOW);
-			break;
-
-		case 3: // 011 Mix1
-			digitalWrite(SEL2, LOW);
-			digitalWrite(SEL1, HIGH);
-			digitalWrite(SEL0, HIGH);
-			break;
-
-		case 4: // 100 Mix2
-			digitalWrite(SEL2, HIGH);
-			digitalWrite(SEL1, LOW);
-			digitalWrite(SEL0, LOW);
-			break;
-
-		case 5: // 101 Mix3
-			digitalWrite(SEL2, HIGH);
-			digitalWrite(SEL1, LOW);
-			digitalWrite(SEL0, HIGH);
-			break;
-	
-		default:
-			break;
-	}
-
-    for(int i = 0; i < 50000; i++)
-    {
-        digitalWrite(STEP_IN, HIGH);
-        delayMicroseconds(del);
-        digitalWrite(STEP_IN, LOW);
-        delayMicroseconds(del);
-    }
-
-	digitalWrite(STEP_IN, LOW);
-	digitalWrite(SEL2, LOW);
-	digitalWrite(SEL1, LOW);
-	digitalWrite(SEL0, LOW);
-}
-
-void make_drink()
-{
-	eject_cup();
-	delay(500);
-	activate_pump(0);
-	activate_pump(3)
-}
-
-void home()
-{
-	// rotate in
-	while (digitalRead(ROT_STOP))
-	{
-		// rotate in
-	}
-	// close clamp
-	// move_clamp("Close");
-	// move y to endstop
-	while (digitalRead(Y_STOP))
-	{
-		y.move(1, 50);
-	}
-	// open clamp
-	move_clamp("Open");
-	// move x to endstop
-	while (digitalRead(X_STOP))
-	{
-		x.move(1, 50);
-	}
-}
-
-void deliver(auto address)
-{
-	// home
-	home();
-	// close clamp
-	move_clamp("Close");
-	// raise y
-	y.move(0, 25000);
-	// move x to position
-	x.move(1, 25000);
-	// rotate out
-	// lower y
-	y.move(1. 25000);
-	// open clamp
-	move_clamp("Open");
-	// raise y
-	y.move(0, 25000);
-	// home again
-	home();
-}
+using namespace SmartBar;
 
 int main(int argc, char* argv[])
 {
+
 	// Initialize WiringPi
 	wiringPiSetup();
 
-	// Set pump pins
-	pinMode(STEP_IN, OUTPUT);
-	pinMode(SEL2, OUTPUT);
-	pinMode(SEL1, OUTPUT);
-	pinMode(SEL0, OUTPUT);
+	// Initialize SmartBar
+	initialize();
 
-	// Set endstop pins
-	pinMode(X_STOP, INPUT);
-	pinMode(Y_STOP, INPUT);
-	pinMode(ROT_STOP, INPUT);
-	pinMode(CLAMP_STOP, INPUT);
-	pullUpDnControl(X_STOP, PUD_UP);
-	pullUpDnControl(Y_STOP, PUD_UP);
-	pullUpDnControl(ROT_STOP, PUD_UP);
-	pullUpDnControl(CLAMP_STOP, PUD_UP);
+	// Test Everything
+	std::cout << "----- Testing System -----" << std::endl;
+	std::cout << "--- Testing Switches ---" << std::endl;
+	/* Switches */
+	bool switch_test = true;
+	while (switch_test)
+	{
+		Switch::read_switch(0);
+		Switch::read_switch(1);
+		Switch::read_switch(2);
+		Switch::read_switch(3);
+		if (Switch::x_isClicked && Switch::y_isClicked)
+		{
+			switch_test = false;
+		}
+		else
+		{
+			if (Switch::x_isClicked)
+			{
+				std::cout << "X Clicked" << std::endl;
+			}
+			if (Switch::y_isClicked)
+			{
+				std::cout << "Y Clicked" << std::endl;
+			}
 
-	make_drink();
-	deliver();
+			if (Switch::rot_isClicked)
+			{
+				std::cout << "Rotation Clicked" << std::endl;
+			}
+
+			if (Switch::clamp_isClicked)
+			{
+				std::cout << "Clamp Clicked" << std::endl;
+			}
+		}
+	}
+	std::cout << "--- Switches Done ---\n" << std::endl;
+
+	std::cout << "--- Testing Bar ---" << std::endl;
+	/* Bar */
+	std::cout << "--- Testing Pumps ---" << std::endl;
+	/* Run Pumps */
+	bool pump_test = true;
+	while (pump_test)
+	{
+		Switch::read_switch(0);
+		if (Switch::x_isClicked)
+		{
+			pump_test = false;
+		}
+		else
+		{
+			// Pump 1
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(0);
+			}
+
+			delay(500);
+			// Pump 2
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(1);
+			}
+
+			delay(500);
+			// Pump 3
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(2);
+			}
+
+			delay(500);
+			// Pump 4
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(3);
+			}
+
+			delay(500);
+			// Pump 5
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(4);
+			}
+
+			delay(500);
+			// Pump 6
+			Switch::read_switch(3);
+			while (!Switch::clamp_isClicked)
+			{
+				Switch::read_switch(3);
+				activate_pump(5);
+			}
+		}
+	}
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Testing Cup Ejection ---" << std::endl;
+	/* Test Cup Ejection */
+	bool eject_test = true;
+	while (eject_test)
+	{
+		Switch::read_switch(0);
+		if (Switch::x_isClicked)
+		{
+			eject_test = false;
+		}
+		else
+		{
+			Switch::read_switch(3);
+			if (Switch::clamp_isClicked)
+			{
+				Bar::eject_cup();
+			}
+		}
+	}
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Testing Make Drink ---" << std::endl;
+	/* Make a drink */
+	std::map<std::string, int> recipe;
+	recipe.insert(std::pair<std::string, int>("Rum", 1));
+	recipe.insert(std::pair<std::string, int>("Coke", 1));
+	bool drink_test = true;
+	while (drink_test)
+	{
+		Switch::read_switch(0);
+		if (Switch::x_isClicked)
+		{
+			drink_test = false;
+		}
+		else
+		{
+			Switch::read_switch(3);
+			if (Switch::clamp_isClicked)
+			{
+				Bar::make_drink(recipe);
+			}
+		}	
+	}
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Bar Done ---\n" << std::endl;
+
+	std::cout << "--- Testing Delivery ---" << std::endl;
+	/* Delivery */
+	std::cout << "--- Testing Motors ---" << std::endl;
+	/* Move Motors */
+	int click_count = 0;
+	while (true)
+	{
+		Switch::read_switch(3);
+		if (Switch::clamp_isClicked && click_count == 0)
+		{
+			Delivery::move_motor("X", "OUT");
+			click_count++;
+		}
+		else if (Switch::clamp_isClicked && click_count == 1)
+		{
+			Delivery::move_motor("Y", "UP");
+			click_count++;
+		}
+		else if (Switch::clamp_isClicked && click_count == 2)
+		{
+			Delivery::move_motor("Rotate", "OUT");
+			click_count++;
+		}
+		else if (Switch::clamp_isClicked && click_count == 3)
+		{
+			Delivery::move_motor("Clamp", "OUT");
+			click_count++;
+		}
+		else if (click_count == 4)
+		{
+			break;
+		}
+	}
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Testing Home ---" << std::endl;
+	/* Home */
+	while (true)
+	{
+		Switch::read_switch(3);
+		if (Switch::clamp_isClicked)
+		{
+			break;
+		}
+	}
+	Delivery::home();
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Testing Deliver ---" << std::endl;
+	/* Deliver */	
+	while (true)
+	{
+		Switch::read_switch(3);
+		if (Switch::clamp_isClicked)
+		{
+			break;
+		}
+	}
+	Delivery::deliver();
+	std::cout << "--- Done ---" << std::endl;
+	std::cout << "--- Delivery Done ---\n" << std::endl;
+	std::cout << "----- Testing Done -----\n" << std::endl;
 }
